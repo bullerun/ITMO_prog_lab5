c6fc3168db0f3362b707095d6b85dbb197d3c623
nikitosek <ryskalnikita2004@mail.ru>
2023-02-10 19:36:37 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         args = new String[]{"123"};
         System.out.println("Hello world!");
-
         System.out.println(args.length);
     }
 }
